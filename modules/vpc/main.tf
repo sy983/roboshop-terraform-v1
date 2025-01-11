f73f94a20d19d4ffa07db43a7364063aac300f88
admin <suhasy983@gmail.com>
2025-01-12 00:28:13 +0530

    roboshop terrform  route table   creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -58,6 +58,11 @@ resource "aws_route_table" "public" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
   tags = {
     Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
   }
@@ -114,4 +119,13 @@ resource "aws_route_table_association" "db" {
   count = length(var.db_subnets)
   subnet_id      = aws_subnet.db.*.id[count.index]
   route_table_id = aws_route_table.db.*.id[count.index]
+}
+
+## internet GW
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
 }
