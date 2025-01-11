1673795dd6417f8079bdc281237fb331fd70be3b
admin <suhasy983@gmail.com>
2025-01-11 23:49:53 +0530

    roboshop terrform  subnet  creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public" {
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "public_subnets"
+    Name = "public_subnets-${split("-", var.availability_zones[count.index])[2]}"
   }
 }
 
@@ -26,7 +26,7 @@ resource "aws_subnet" "web" {
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "web_subnets"
+    Name = "web_subnets-${split("-", var.availability_zones[count.index])[2]}"
   }
 }
 
@@ -37,7 +37,7 @@ resource "aws_subnet" "app" {
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "app_subnets"
+    Name = "app_subnets-${split("-", var.availability_zones[count.index])[2]}"
   }
 }
 
@@ -48,6 +48,23 @@ resource "aws_subnet" "db" {
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "db_subnets"
+    Name = "db_subnets-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+
+## Route tables
+
+resource "aws_route_table" "public" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+
+  tags = {
+    Name = "example"
   }
 }
