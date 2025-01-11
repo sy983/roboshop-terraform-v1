c7106804dea31312bf92d1ee5089937bc9901ed7
admin <suhasy983@gmail.com>
2025-01-11 23:51:02 +0530

    roboshop terrform  subnet  creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -54,17 +54,17 @@ resource "aws_subnet" "db" {
 
 ## Route tables
 
-resource "aws_route_table" "public" {
-  count = length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
-  }
-
-
-  tags = {
-    Name = "example"
-  }
-}
+# resource "aws_route_table" "public" {
+#   count = length(var.public_subnets)
+#   vpc_id = aws_vpc.main.id
+#
+#   route {
+#     cidr_block = "10.0.1.0/24"
+#     gateway_id = aws_internet_gateway.example.id
+#   }
+#
+#
+#   tags = {
+#     Name = "example"
+#   }
+# }
