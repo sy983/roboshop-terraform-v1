2da8b98078fd243d4fa43a7ab5544d0744222390
admin <suhasy983@gmail.com>
2025-02-02 16:41:33 +0530

    EC2 module creation

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 pip3.11 install ansible hvac
-ansible-pull -i localhost, -U https://github.com/sy983/roboshop-ansible.git-v1.git main.yml -e env="$(env)" -e role_name="$(role_name)" -e vault_token="$(vault_token)" 2>&1| tee /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/sy983/roboshop-ansible.git-v1.git main.yml -e env="${env}" -e role_name="${role_name}" -e vault_token="${vault_token}" 2>&1 | tee /opt/userdata.log
