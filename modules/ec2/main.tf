ad83905a302c6ed74567abc53ad06a1191198955
admin <suhasy983@gmail.com>
2025-02-15 20:39:57 +0530

    EC2 module creation

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -37,6 +37,10 @@ resource "aws_launch_template" "main" {
   image_id = data.aws_ami.rhel9.id
   instance_type =  var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data     = base64encode(templatefile("${path.module}/userdata.sh" , {
+    env         =  var.env
+    role_name   =  var.name
+    vault_token =  var.vault_token
 
   tags = {
     Name = "${var.name}-${var.env}-lt"
