3e3e438a53fafa763ccc97c608f87bbfb821f910
admin <suhasy983@gmail.com>
2025-02-16 22:35:17 +0530

    EC2 module creation for load balancer

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "db" {
   instance_type = each.value["instance_type"]
   allow_port    = each.value["allow_port"]
   allow_sg_cidr = each.value["allow_sg_cidr"]
-  subnet_ids    = module.vpc.subnets [each.value["subnet_ref"]]
+  subnet_ids    = module.vpc.subnets[each.value["subnet_ref"]]
   vpc_id        = module.vpc.vpc_id
   env           = var.env
   bastion_nodes = var.bastion_nodes
