837a299cb0d29346a36e3c88c04222809b8498fe
admin <suhasy983@gmail.com>
2025-02-22 06:41:09 +0530

    EC2 module creation for load balancer

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -144,7 +144,7 @@ resource "aws_security_group" "load-balancer" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = var.allow_sg_cidr
+    cidr_blocks      = var.name == "frontend" ? ["0.0.0.0/0"] : var.allow_sg_cidr
   }
 
   tags = {
