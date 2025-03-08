bbd883f2b4cf3afa780c07ffafbef7fd190096b5
admin <suhasy983@gmail.com>
2025-03-08 16:03:19 +0530

    sapareted asg module and added https cert and url redirection

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "main" {
  ami           = data.aws_ami.rhel9.image_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_ids[0]
-  vpc_security_group_ids = [aws_security_group.allow_tls]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   user_data     = base64encode(templatefile("${path.module}/userdata.sh" , {
     env         =  var.env
     role_name   =  var.name
