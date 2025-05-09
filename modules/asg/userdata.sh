ca65840e8770577c85acae6783091dd3ae92c862
admin <suhasy983@gmail.com>
2025-05-09 08:49:18 +0530

    update docker yml

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -8,3 +8,5 @@ pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/sy983/roboshop-ansible.git-v1.git main.yml -e env="${env}" -e role_name=docker -e app_name=${app_name} -e vault_token="${vault_token}" 2>&1 | tee -a /opt/userdata.log
 
 
+
+
