24952f9f3fb0b64c2eca8b71f38289f2017a7d24
admin <suhasy983@gmail.com>
2025-05-09 08:25:58 +0530

    update docker yml

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -4,4 +4,6 @@
 dnf install docker -y
 growpart /dev/nvme0n1 4
 lvextend -r -L +10G /dev/mapper/RootVG-varVol
+ansible-pull -i localhost, -U https://github.com/sy983/roboshop-ansible.git-v1.git main.yml -e env="${env}" -e role_name=docker -e app_name=${app_name} -e vault_token="${vault_token}" 2>&1 | tee /opt/userdata.log
+
 
