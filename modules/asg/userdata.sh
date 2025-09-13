dee776c981dad0693dc5c405e9052c67eaf3b842
suhas yewale <suhasy983@gmail.com>
2025-09-13 19:44:03 +0530

    updated userdata.sh for docker

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -1,4 +1,4 @@
-##!/bin/bash
+#!/bin/bash
 ### Below to use ec2
 #pip3.11 install ansible hvac
 #ansible-pull -i localhost, -U https://github.com/sy983/roboshop-ansible.git-v1.git main.yml -e env="${env}" -e  role_name="${role_name}" -e vault_token="${vault_token}" 2>&1 | tee /opt/userdata.log
